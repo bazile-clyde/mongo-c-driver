2b624f55ef29e9973fd9a184f9755490de3ba0fb
Clyde Bazile <bazileclyde@gmail.com>
2020-04-16 11:58:18 -0400

    specify test

---
+++ .evergreen/run-ocsp-cache-test.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# start valid OCSP responder
+
+# cert should be valid
+
+# shutdown valid and start revoke OCSP responder
+
+# cert should be cached and still valid
+
+# increase clock time to outside of cert's range (>= 20 years)
+
+# cert should be revoked
