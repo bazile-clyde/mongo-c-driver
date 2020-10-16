c7741ded7fb8a67c478bf8b050bfdad7e2695c80
Clyde Bazile <clyde.bazile@10gen.com>
2020-10-16 16:37:05 -0400

    add server selection timeout test that fails

--- src/libmongoc/tests/test-mongoc-timeout-ms-errors.c
+++ src/libmongoc/tests/test-mongoc-timeout-ms-errors.c
@@ -1,9 +1,34 @@
 #include "TestSuite.h"
+#include "test-conveniences.h"
 
 static void
 test_server_selection_timeout (void)
 {
-   ASSERT(true);
+   mongoc_uri_t *uri;
+   mongoc_client_t *client;
+   mongoc_collection_t *collection;
+   bson_error_t error;
+   bson_t *command;
+   bson_t reply;
+   bool result;
+
+   uri = mongoc_uri_new ("mongodb://localhost:11111");
+   ASSERT (uri);
+
+   mongoc_uri_set_option_as_int32 (uri, MONGOC_URI_SERVERSELECTIONTIMEOUTMS, 1);
+   mongoc_uri_set_option_as_bool (uri, MONGOC_URI_SERVERSELECTIONTRYONCE, false);
+
+   client = mongoc_client_new_from_uri(uri);
+   collection = mongoc_client_get_collection (client, "test", "test");
+   command = tmp_bson ("{'ping': 1}");
+   result = mongoc_collection_command_simple (
+      collection, command, NULL, &reply, &error);
+   ASSERT (!result);
+
+   bson_destroy (&reply);
+   mongoc_collection_destroy (collection);
+   mongoc_client_destroy (client);
+   mongoc_uri_destroy (uri);
 }
 
 void
